7879a574eeb3b2804ecacde406122a56285e038f
Luke Parker <107251675+LukeRParker@users.noreply.github.com>
2023-05-16 21:04:53 +0100

    Update bigbangdev.sh

--- bigbangdev.sh
+++ bigbangdev.sh
@@ -4,21 +4,17 @@
 sudo su
 sudo apt update
 sudo apt upgrade -y
+sudo apt install pip
 
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9
-
-python3.9 -m pip install --upgrade pip
-python3.9 pip install pandas
-python3.9 pip install numpy
-python3.9 pip install langchain
-python3.9 pip install llama-cpp-python 
-python3.9 pip install huggingface-hub 
-python3.9 pip install openai
-python3.9 pip install sqlalchemy 
-python3.9 pip install scikit-learn 
-python3.9 pip install wikipedia
+pip install pandas
+pip install numpy
+pip install langchain
+pip install llama-cpp-python 
+pip install huggingface-hub 
+pip install openai
+pip install sqlalchemy 
+pip install scikit-learn 
+pip install wikipedia
 
 
 git lfs clone https://huggingface.co/TheBloke/GPT4All-13B-snoozy-GGML/blob/main/GPT4All-13B-snoozy.ggml.q5_0.bin
